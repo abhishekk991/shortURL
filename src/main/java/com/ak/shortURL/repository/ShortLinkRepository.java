b18a0ce62a3cc8965ef42d4b9d89bd097a317292
abhishekk991 <abhishekk991@gmail.com>
2024-12-14 15:13:05 -0600

    Able to create new and retrieve short and long urls if already present in DB

--- src/main/java/com/ak/shortURL/repository/ShortLinkRepository.java
+++ src/main/java/com/ak/shortURL/repository/ShortLinkRepository.java
@@ -2,6 +2,11 @@ package com.ak.shortURL.repository;
 
 import com.ak.shortURL.model.ShortLinkModel;
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
 public interface ShortLinkRepository extends CrudRepository<ShortLinkModel, Long> {
+
+    Optional<ShortLinkModel> findByLongUrl(String longUrl);
+
+    Optional<ShortLinkModel> findByShortUrl(String shortUrl);
 }
